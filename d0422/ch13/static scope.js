9bd82c8854f52923f03fa023b3528bb773e36865
d0422 <rlfehd2013@naver.com>
2023-01-09 22:25:58 +0900

    CH13 + code

---
+++ d0422/ch13/static scope.js
@@ -0,0 +1,10 @@
+var x = 1;
+function foo() {
+  var x = 10;
+  bar();
+}
+function bar() {
+  console.log(x);
+}
+foo(); //1
+bar(); //1
---
+++ d0422/ch13/var,let.js
@@ -0,0 +1,11 @@
+var i = 10;
+for (var i = 0; i < 5; i++) {
+  console.log(i);
+}
+console.log(i); //5
+
+let j = 10;
+for (let j = 0; j < 5; j++) {
+  console.log(j);
+}
+console.log(j); //10
