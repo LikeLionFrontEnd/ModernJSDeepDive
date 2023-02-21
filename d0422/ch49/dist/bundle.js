1db610491eecca9a08193bb9bc1af81ea94a820e
d0422 <rlfehd2013@naver.com>
2023-02-21 16:28:39 +0900

    webpack, babel

---
+++ d0422/ch49/dist/bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*********************!*\
+  !*** ./src/main.js ***!
+  \*********************/
+console.log('hi');
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ d0422/ch49/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [path.resolve(__dirname, 'src')],
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  devtool: 'source-map',
+  mode: 'development',
+};
